6ac6704e9a82137c8d34c4503243cd81c6445e12
Cavlon <zmzf47@durham.ac.uk>
2023-12-13 21:04:41 +0000

    Server now uses dictionary and comments added

--- client.py
+++ client.py
@@ -2,45 +2,59 @@ import socket
 import sys
 from threading import Thread
 
-end = False
-
 def connect():
-    USER = sys.argv[1]
+    USER = sys.argv[1] # The client's name
     HOST = sys.argv[2]  # The server's hostname or IP address
     PORT = int(sys.argv[3])  # The port used by the server
 
+    # Create a socket and connect to the server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
+    # Initialise by sending the user's name
     s.sendall(USER.encode())
 
+    # Separate the sending and receiving functionality with a new thread
     send = Thread(target=sender, args=(s,))
     send.start()
     receiver(s)
     send.join()
+
+    # Once both the receiver and sender functions end, close the socket
     s.close()
 
 def sender(s):
+    # Keep requesting inputs until an error or exit
     while True:
         message = input('Write your message: ')
         try:
+            # Send the input message to the server
             s.sendall(message.encode())
+
         except socket.error:
+            # Safely exit if there was a forceful disconnection
             break
+        
+        # End sending functionality if client exits
         if message == '/exit':
             break
     return
 
 def receiver(s):
-    global end
-    while end == False:
+    # Keep receiving messages until an error or notified of an exit
+    while True:
         try:
+            # Receive a message from the server
             data = s.recv(1024).decode()
+
         except socket.error:
+            # Safely exit if there was a forceful disconnection
             print('\n>>Disconnected from server')
             break
 
+        # End receiving functionality if client exits
         if data == '/exit':
             break
+
         print(f'\n{data}')
     return
 
--- server.py
+++ server.py
@@ -4,102 +4,136 @@ from threading import Thread
 
 
 PORT = int(sys.argv[1])
-all_conns = []
-all_addr = []
-all_names = []
+# Holds the address, connection and name of each connected client
+client_info = dict()
 
-def broadcast(message, exclude=None):       
-    for connection in all_conns:
-        if exclude:
-            if connection == exclude:
-                continue
-        connection.sendall(message.encode())
+# Send a message to every connected client
+def broadcast(message, addr = None):       
+    for client in client_info.keys():
+        # Don't send the message to the sender if they are specified
+        if client == addr:
+            continue
+        client_info[client][0].sendall(message.encode())
 
-def disconnect(conn, addr, name):
+# Runs whenever a client disconnects
+def disconnect(addr):
+    name = client_info[addr][1]
     leaveMessage = '>>' + name + ' has left'
     print(f'Disconnection by {name} from {addr}')
-    all_conns.remove(conn)
-    all_addr.remove(addr)
-    all_names.remove(name)
+    client_info.pop(addr)
+    # Sends a leave message to all other clients
     broadcast(leaveMessage)
 
-def on_client_connect(conn, addr, name):
-    targetconn = None
-    ind = 0
+def active_client(addr):
+    targetaddr = None
+    conn = client_info[addr][0]
+    name = client_info[addr][1]
+
+    # Keep waiting for client data until they disconnect or exit
     while True:
+
         try:
+            # Wait for a message from the client
             data = conn.recv(1024).decode()
+
+        #In case of forceful disconnection
         except socket.error:
-            disconnect(conn, addr, name)
+            disconnect(addr)
             conn.close()
             return
         
         print(f'received>> {data}')
+
+        # Client requests a disconnection
         if data == '/exit':
             break
 
-        elif data.startswith('/uni'):
+        # Client requests to unicast to a specific address
+        if data.startswith('/uni'):
             try:
-                params = data.split()
-                targetaddr = (params[1], int(params[2]))
-                if targetaddr == addr:
-                    conn.sendall("You can't send messages to yourself".encode())
-                    continue
-                ind = all_addr.index(targetaddr)
-                targetconn = all_conns[ind]
-                conn.sendall(f'Directly Connected to {all_names[ind]}'.encode())
-            except ValueError:
+                if len(client_info) > 1:
+                    params = data.split()
+                    temp = (params[1], int(params[2]))
+
+                    if temp == addr:
+                        conn.sendall("You can't send messages to yourself".encode())
+                        continue
+
+                    conn.sendall(f'Directly Connected to {client_info[temp][1]}'.encode())
+                    targetaddr = temp
+                
+            except ValueError:  # If the port number isn't a number
                 conn.sendall('Invalid command'.encode())
-            except IndexError:
+            except IndexError:  # If there are too little parameters
                 conn.sendall('Invalid command'.encode())
+            except KeyError:    # If the specified address doesn't exist
+                conn.sendall("Address doesn't exist".encode())
             continue
 
-        elif data == '/broad':
-            targetconn = None
+        # Client requests to broadcast
+        if data == '/broad':
+            targetaddr = None
             continue
 
-        elif data == '/members':
+        # Client requests a list of all the connected clients
+        if data == '/members':
             message = 'List of members and their ports:\n'
-            for i in range(len(all_conns)):
-                message += f'{all_names[i]} at port {all_addr[i]}\n'
+
+            for client in client_info.keys():
+                message += f'{client_info[client][1]} at address {client}\n'
+
             conn.sendall(message.encode()) 
             continue
 
-        if targetconn:
-            print(f'sending data to {all_names[ind]} at {all_addr[ind]}')
-            data = f'{name} (whisper)>>' + data
-            targetconn.sendall(data.encode()) 
-        else:
-            print(f'sending data back to everyone')
-            data = f'{name}>>' + data
-            broadcast(data, conn)
+        # Unicast to the selected target if it still exists
+        if targetaddr:
+            if targetaddr in client_info:
+                print(f'Sending data to {client_info[targetaddr][1]} at {targetaddr}')
+                data = f'{name} (whisper)>>' + data
+                client_info[targetaddr][0].sendall(data.encode()) 
+                continue
+            conn.sendall(">>Unicast target doesn't exist, switching to broadcast".encode())
+            targetaddr = None
 
+        # Broadcast if the message wasn't a command and there isn't a unicast target
+        print(f'sending data back to everyone')
+        data = f'{name}>>' + data
+        broadcast(data, addr)
+
+    # If the client requests to disconnect
     conn.sendall(f'>>Goodbye {name}'.encode())
-    disconnect(conn, addr, name)
+    disconnect(addr)
 
+    # Notify the disconnecting client that the prerequistes for disconnection is complete
     conn.sendall('/exit'.encode())
     conn.close()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', PORT))
-s.listen(5)
+def init():
+    # Create a socket and bind the address info
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', PORT))
+    s.listen(5)
 
-while True:
-    print("Waiting for Connection")
+    # Keep waiting for new connections
+    while True:
+        print("Waiting for Connection")
 
-    conn, addr = s.accept()
-    name = conn.recv(1024).decode()
+        # Extract and store client information from the new connection
+        conn, addr = s.accept()
+        name = conn.recv(1024).decode()
+        info = (conn, name)
 
-    print(f"Connected by {name} from {addr}")
-    conn.sendall(f'>>Welcome {name}!'.encode())
+        # Notify of the successful connection
+        print(f"Connected by {name} from {addr}")
+        conn.sendall(f'>>Welcome {name}!'.encode())
+        welcMessage = '>>' + name + ' has joined'
+        broadcast(welcMessage)
+        
+        client_info.update({addr:info})
 
-    welcMessage = '>>' + name + ' has joined'
-    for connection in all_conns:
-        connection.sendall(welcMessage.encode())
+        # Dedicate a unique thread to each connected client
+        t = Thread(target=active_client, args=(addr, ))
+        t.start()
 
-    all_conns.append(conn)
-    all_addr.append(addr)
-    all_names.append(name)
-
-    t = Thread(target=on_client_connect, args=(conn, addr, name))
-    t.start()
+if __name__ == "__main__":
+    init()
